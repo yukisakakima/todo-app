dde4067d2cfdb2c8143d29fba25bc42dd08f159d
yukisakakima <kima87161211@gmail.com>
2023-04-04 14:38:01 +0900

    完了未完了の実装が完了致しました

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ $("#addTodo").click(function(){
     $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
 
     $("input").val("");
+});
+
+$(document).on("change", "input[type=checkbox]", function() {
+    if($(this).is(":checked")){
+        $(this).parent().css("text-decoration", "line-through");
+    } else {
+        $(this).parent().css("text-decoration", "none");
+    }
 })
